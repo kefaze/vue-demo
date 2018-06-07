6b1dae97bfc757f115f895a3769914985dfeb893
kefaze <1285973761@qq.com>
2018-06-08 07:26:09 +0800

    123

--- src/pages/detial.vue
+++ src/pages/detial.vue
@@ -4,7 +4,7 @@
       <div class="product-board">
         <img :src="productIcon">
         <ul>
-          <router-link v-for="item in products" :to="{ path: item.path }" tag="li" active-class="active">
+          <router-link v-for="(item,index) in products" :to="{ path: item.path }" :key="index" tag="li" active-class="active">
             {{ item.name }}
           </router-link>
         </ul>
--- src/pages/detial/analysis.vue
+++ src/pages/detial/analysis.vue
@@ -1,294 +1,430 @@
 <template>
   <div class="sales-board">
-      <div class="sales-board-intro">
-        <h2>流量分析</h2>
-        <p>是指在获得网站访问量基本数据的情况下对有关数据进行统计、分析，从中发现用户访问网站的规律，并将这些规律与网络营销策略等相结合，从而发现目前网络营销活动中可能存在的问题，并为进一步修正或重新制定网络营销策略提供依据。当然这样的定义是站在网络营销管理的角度来考虑的</p>
-      </div>
-      <div class="sales-board-form">
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">
-                  购买数量：
-              </div>
-              <div class="sales-board-line-right">
-                <v-counter @on-change="onParamChange('buyNum', $event)"></v-counter>
-              </div>
-          </div>
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">
-                  产品类型：
-              </div>
-              <div class="sales-board-line-right">
-                  <Vselection :selections="buyTypes"  @on-change="onParamChange('buyType', $event)"></Vselection>
-              </div>
-          </div>
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">
-                  有效时间：
-              </div>
-              <div class="sales-board-line-right">
-                  <v-chooser
-                  :selections="periodList"
-                  @on-change="onParamChange('period', $event)"></v-chooser>
-              </div>
-          </div>
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">
-                  产品版本：
-              </div>
-              <div class="sales-board-line-right">
-                  <v-mul-chooser
-                  :selections="versionList"
-                  @on-change="onParamChange('versions', $event)"></v-mul-chooser>
-              </div>
-          </div>
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">
-                  总价：
-              </div>
-              <div class="sales-board-line-right">
-                  {{ price }} 元
-              </div>
-          </div>
-          <div class="sales-board-line">
-              <div class="sales-board-line-left">&nbsp;</div>
-              <div class="sales-board-line-right">
-                  <div class="button" @click="payClick">
-                    立即购买
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="sales-board-des">
-        <h2>产品说明</h2>
-        <p>网站访问统计分析报告的基础数据源于网站流量统计信息，但其价值远高于原始数据资料。专业的网站访问统计分析报告对网络营销的价值，正如专业的财务分析报告对企业经营策略的价值。</p>
-
-        <h3>用户行为指标</h3>
-        <ul>
-          <li>用户行为指标主要反映用户是如何来到网站的、在网站上停留了多长时间、访问了哪些页面等，主要的统计指标包括：</li>
-          <li>用户在网站的停留时间；</li>
-          <li>用户来源网站（也叫“引导网站”）；</li>
-          <li>用户所使用的搜索引擎及其关键词；</li>
-          <li>在不同时段的用户访问量情况等。</li>
-        </ul>
-
-        <h3>浏览网站方式</h3>
-        <ul>
-          <li>用户上网设备类型</li>
-          <li>用户浏览器的名称和版本</li>
-          <li>访问者电脑分辨率显示模式</li>
-          <li>用户所使用的操作系统名称和版本</li>
-          <li>用户所在地理区域分布状况等</li>
-        </ul>
-      </div>
-
-      <Dialog :is-show="isShowPayDialog" @on-close="hidePayDialog">
-        <table class="buy-dialog-table">
-          <tr>
-            <th>购买数量</th>
-            <th>产品类型</th>
-            <th>有效时间</th>
-            <th>产品版本</th>
-            <th>总价</th>
-          </tr>
-          <tr>
-            <td>{{ buyNum }}</td>
-            <td>{{ buyType.label }}</td>
-            <td>{{ period.label }}</td>
-            <td>
-              <span v-for="item in versions">{{ item.label }}</span>
-            </td>
-            <td>{{ price }}</td>
-          </tr>
-        </table>
-        <h3 class="buy-dialog-title">请选择银行</h3>
-
-        <bank-chooser @on-change="onChangeBank"></bank-chooser>
-        
-        <div class="button buy-dialog-btn" @click="confirmBuy">
-          确认购买
+    <div class="sales-board-intro">
+      <h2>流量分析</h2>
+      <p>是指在获得网站访问量基本数据的情况下对有关数据进行统计、分析，从中发现用户访问网站的规律，并将这些规律与网络营销策略等相结合，从而发现目前网络营销活动中可能存在的问题，并为进一步修正或重新制定网络营销策略提供依据。当然这样的定义是站在网络营销管理的角度来考虑的</p>
+    </div>
+    <div class="sales-board-form">
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">
+          购买数量：
         </div>
-      </Dialog>
+        <div class="sales-board-line-right">
+          <v-counter @on-change="onParamChange('buyNum', $event)"></v-counter>
+        </div>
+      </div>
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">
+          产品类型：
+        </div>
+        <div class="sales-board-line-right">
+          <Vselection :selections="buyTypes" @on-change="onParamChange('buyType', $event)"></Vselection>
+        </div>
+      </div>
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">
+          有效时间：
+        </div>
+        <div class="sales-board-line-right">
+          <v-chooser :selections="periodList" @on-change="onParamChange('period', $event)"></v-chooser>
+        </div>
+      </div>
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">
+          产品版本：
+        </div>
+        <div class="sales-board-line-right">
+          <v-mul-chooser :selections="versionList" @on-change="onParamChange('versions', $event)"></v-mul-chooser>
+        </div>
+      </div>
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">
+          总价：
+        </div>
+        <div class="sales-board-line-right">
+          {{ price }} 元
+        </div>
+      </div>
+      <div class="sales-board-line">
+        <div class="sales-board-line-left">&nbsp;</div>
+        <div class="sales-board-line-right">
+          <div class="button" @click="payClick">
+            立即购买
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sales-board-des">
+      <h2>产品说明</h2>
+      <p>网站访问统计分析报告的基础数据源于网站流量统计信息，但其价值远高于原始数据资料。专业的网站访问统计分析报告对网络营销的价值，正如专业的财务分析报告对企业经营策略的价值。</p>
 
-      <Dialog :is-show="isShowErrDialog" @on-close="hideErrDialog">
-        支付失败！
-      </Dialog>
+      <h3>用户行为指标</h3>
+      <ul>
+        <li>用户行为指标主要反映用户是如何来到网站的、在网站上停留了多长时间、访问了哪些页面等，主要的统计指标包括：</li>
+        <li>用户在网站的停留时间；</li>
+        <li>用户来源网站（也叫“引导网站”）；</li>
+        <li>用户所使用的搜索引擎及其关键词；</li>
+        <li>在不同时段的用户访问量情况等。</li>
+      </ul>
 
-      <check-order :is-show-check-dialog="isShowCheckOrder" :order-id="orderId" 
-      @on-close-check-dialog="hideCheckOrder"></check-order> 
+      <h3>浏览网站方式</h3>
+      <ul>
+        <li>用户上网设备类型</li>
+        <li>用户浏览器的名称和版本</li>
+        <li>访问者电脑分辨率显示模式</li>
+        <li>用户所使用的操作系统名称和版本</li>
+        <li>用户所在地理区域分布状况等</li>
+      </ul>
+    </div>
+
+    <Dialog :is-show="isShowPayDialog" @on-close="hidePayDialog">
+      <table class="buy-dialog-table">
+        <tr>
+          <th>购买数量</th>
+          <th>产品类型</th>
+          <th>有效时间</th>
+          <th>产品版本</th>
+          <th>总价</th>
+        </tr>
+        <tr>
+          <td>{{ buyNum }}</td>
+          <td>{{ buyType.label }}</td>
+          <td>{{ period.label }}</td>
+          <td>
+            <span v-for="item in versions">{{ item.label }}</span>
+          </td>
+          <td>{{ price }}</td>
+        </tr>
+      </table>
+      <h3 class="buy-dialog-title">请选择银行</h3>
+
+      <bank-chooser @on-change="onChangeBank"></bank-chooser>
+
+      <div class="button buy-dialog-btn" @click="confirmBuy">
+        确认购买
+      </div>
+    </Dialog>
+
+    <Dialog :is-show="isShowErrDialog" @on-close="hideErrDialog">
+      支付失败！
+    </Dialog>
+
+    <check-order :is-show-check-dialog="isShowCheckOrder" :order-id="orderId" @on-close-check-dialog="hideCheckOrder"></check-order>
+
+    <h3>列表</h3>
+    <div class="site_table">
+      <table width="120px" v-for="item in siteType">
+        <tr style="height:40px;text-align: center">
+          <td>{{item.name}}</td>
+          <td>:</td>
+          <td>{{item.count}}</td>
+        </tr>
+      </table>
+    </div>
+
+    <hr>
+    <div class="table-row-group">
+      <ul class="table-row" v-for="item in siteType">
+        <li class="table-cell">{{item.name}}</li>
+        <li class="table-cell">:</li>
+        <li class="table-cell">{{item.count}}</li>
+      </ul>
+    </div>
+
+    <hr>
+    <h2>滚动条div</h2>
+    <div class='box' id='wrapper1'>
+      <div class="scroll" id="scroll">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Vselection from '../../components/base/selection'
-import VMulChooser from '../../components/base/multiplyChooser'
-import VChooser from '../../components/base/chooser'
-import VCounter from '../../components/base/counter'
-import _ from 'lodash'
-import Dialog from '../../components/base/Dialog'
-import bankChooser from '../../components/bankChooser'
-import checkOrder from '../../components/checkOrder'
+import '../../js/my_scrollbar.js'
+
+import Vselection from "../../components/base/selection";
+import VMulChooser from "../../components/base/multiplyChooser";
+import VChooser from "../../components/base/chooser";
+import VCounter from "../../components/base/counter";
+import _ from "lodash";
+import Dialog from "../../components/base/Dialog";
+import bankChooser from "../../components/bankChooser";
+import checkOrder from "../../components/checkOrder";
+
+var oMyBar1 = new MyScrollBar({
+    selId: 'wrapper1'
+});
 export default {
-  components: {
-    Vselection,
-    VMulChooser,
-    VChooser,
-    VCounter,
-    Dialog,
-    bankChooser,
-    checkOrder
-  },
-  data () {
-    return {
-      buyNum: 0,
-      buyType: {},
-      versions: [],
-      period: {},
-      price: 0,
-      isShowPayDialog: false,
-      isShowErrDialog: false,
-      isShowCheckOrder: false,
-      bankId: null,
-      orderId: null,
-      buyTypes: [
-        {
-          label: '入门版',
-          value: 0
+    components: {
+        Vselection,
+        VMulChooser,
+        VChooser,
+        VCounter,
+        Dialog,
+        bankChooser,
+        checkOrder
+    },
+    data() {
+        return {
+            siteType: [
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 },
+                { name: "asd", count: 18 }
+            ],
+            buyNum: 0,
+            buyType: {},
+            versions: [],
+            period: {},
+            price: 0,
+            isShowPayDialog: false,
+            isShowErrDialog: false,
+            isShowCheckOrder: false,
+            bankId: null,
+            orderId: null,
+            buyTypes: [
+                {
+                    label: "入门版",
+                    value: 0
+                },
+                {
+                    label: "中级版",
+                    value: 1
+                },
+                {
+                    label: "高级版",
+                    value: 2
+                }
+            ],
+            periodList: [
+                {
+                    label: "半年",
+                    value: 0
+                },
+                {
+                    label: "一年",
+                    value: 1
+                },
+                {
+                    label: "三年",
+                    value: 2
+                }
+            ],
+            versionList: [
+                {
+                    label: "客户版",
+                    value: 0
+                },
+                {
+                    label: "代理商版",
+                    value: 1
+                },
+                {
+                    label: "专家版",
+                    value: 2
+                }
+            ]
+        };
+    },
+    methods: {
+        confirmBuy() {
+            let buyVersionsArray = _.map(this.versions, item => {
+                return item.value;
+            });
+            let reqParams = {
+                buyNumber: this.buyNum,
+                buyType: this.buyType.value,
+                period: this.period.value,
+                version: buyVersionsArray.join(","),
+                bankId: this.bankId
+            };
+            this.$http.get("/api/createOrder", reqParams).then(
+                res => {
+                    console.log(res);
+                    this.orderId = res.data.orderId;
+                    this.isShowPayDialog = false;
+                    this.isShowCheckOrder = true;
+                },
+                err => {
+                    this.isShowErrDialog = true;
+                }
+            );
         },
-        {
-          label: '中级版',
-          value: 1
+        onChangeBank(bankOj) {
+            this.bankId = bankOj.id;
+            console.log(bankOj.id);
         },
-        {
-          label: '高级版',
-          value: 2
+        onParamChange(attr, val) {
+            this[attr] = val;
+            console.log(this[attr], attr);
+            this.getPrice();
+        },
+        getPrice() {
+            let buyVersionsArray = _.map(this.versions, item => {
+                return item.value;
+            });
+            let reqParams = {
+                buyNumber: this.buyNum,
+                buyType: this.buyType.value,
+                period: this.period.value,
+                version: buyVersionsArray.join(",")
+            };
+            this.$http.get("/api/getPrice", reqParams).then(res => {
+                console.log(res);
+                this.price = res.data.amount;
+            });
+        },
+        payClick() {
+            this.isShowPayDialog = true;
+        },
+        hidePayDialog() {
+            this.isShowPayDialog = !this.isShowPayDialog;
+            this.isShowErrDialog = true;
+        },
+        showPayDialog() {
+            this.isShowPayDialog = true;
+        },
+        hideErrDialog() {
+            this.isShowErrDialog = false;
+        },
+        hideCheckOrder() {
+            this.isShowCheckOrder = false;
         }
-      ],
-      periodList: [
-        {
-          label: '半年',
-          value: 0
-        },
-        {
-          label: '一年',
-          value: 1
-        },
-        {
-          label: '三年',
-          value: 2
-        }
-      ],
-      versionList: [
-        {
-          label: '客户版',
-          value: 0
-        },
-        {
-          label: '代理商版',
-          value: 1
-        },
-        {
-          label: '专家版',
-          value: 2
-        }
-      ], 
+    },
+    mounted() {
+        this.buyNum = 1;
+        this.buyType = this.buyTypes[0];
+        this.versions = [this.versionList[0]];
+        this.period = this.periodList[0];
+        this.getPrice();
     }
-  },
-  methods: {
-    confirmBuy () {
-      let buyVersionsArray = _.map(this.versions, (item) => {
-        return item.value
-      })
-      let reqParams = {
-        buyNumber: this.buyNum,
-        buyType: this.buyType.value,
-        period: this.period.value,
-        version: buyVersionsArray.join(','),
-        bankId: this.bankId
-      }
-      this.$http.get('/api/createOrder', reqParams)
-      .then((res) => {
-        console.log(res)
-        this.orderId = res.data.orderId
-        this.isShowPayDialog = false
-        this.isShowCheckOrder = true
-      }, (err) => {
-        this.isShowErrDialog = true
-      })
-    },
-    onChangeBank (bankOj) {
-      this.bankId = bankOj.id
-      console.log(bankOj.id)
-    },
-    onParamChange (attr, val) {
-      this[attr] = val
-      console.log(this[attr], attr)
-      this.getPrice ()
-    },
-    getPrice () {
-      let buyVersionsArray = _.map(this.versions, (item) => {
-        return item.value
-      })
-      let reqParams = {
-        buyNumber: this.buyNum,
-        buyType: this.buyType.value,
-        period: this.period.value,
-        version: buyVersionsArray.join(',')
-      }
-      this.$http.get('/api/getPrice', reqParams)
-      .then((res) => {
-        console.log(res)
-        this.price = res.data.amount
-      })
-    },
-    payClick () {
-      this.isShowPayDialog = true
-    },
-    hidePayDialog () {
-      this.isShowPayDialog = !this.isShowPayDialog
-      this.isShowErrDialog = true
-    },
-    showPayDialog () {
-      this.isShowPayDialog = true
-    },
-    hideErrDialog () {
-      this.isShowErrDialog = false
-    },
-    hideCheckOrder () {
-      this.isShowCheckOrder = false
-    }
-  },
-  mounted () {
-      this.buyNum = 1
-      this.buyType = this.buyTypes[0]
-      this.versions = [this.versionList[0]]
-      this.period = this.periodList[0]
-      this.getPrice ()
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.box {
+    width: 600px; height: 300px; border: 1px solid #444; margin: 10px auto; padding: 0; overflow: hidden;
+}
+
+ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+.table {
+    display: table;
+    border-collapse: collapse;
+    border: 1px solid #ccc;
+}
+.table-row-group {
+    /* display: table-row-group; */
+    width: 600px;
+    height: 300px;
+    border: 1px solid blue;
+    display: flex;
+    overflow: auto;
+    display: flex;
+    flex-wrap: wrap;
+}
+.table-row {
+    display: table-row;
+    text-align: center;
+}
+.table-row-group .table-row:hover,
+.table-footer-group .table-row:hover {
+    background: #f6f6f6;
+}
+.table-cell {
+    display: table-cell;
+    padding: 0 5px;
+    vertical-align: middle;
+}
+.table-header-group {
+    display: table-header-group;
+    background: #eee;
+    font-weight: bold;
+}
+.table-footer-group {
+    display: table-footer-group;
+}
+
 .buy-dialog-title {
-  font-size: 16px;
-  font-weight: bold;
+    font-size: 16px;
+    font-weight: bold;
 }
 .buy-dialog-btn {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 .buy-dialog-table {
-  width: 100%;
-  margin-bottom: 20px;
+    width: 100%;
+    margin-bottom: 20px;
 }
 .buy-dialog-table td,
-.buy-dialog-table th{
-  border: 1px solid #e3e3e3;
-  text-align: center;
-  padding: 5px 0;
+.buy-dialog-table th {
+    border: 1px solid #e3e3e3;
+    text-align: center;
+    padding: 5px 0;
 }
 .buy-dialog-table th {
-  background: #4fc08d;
-  color: #fff;
-  border: 1px solid #4fc08d;
+    background: #4fc08d;
+    color: #fff;
+    border: 1px solid #4fc08d;
+}
+
+.site_table {
+    width: 560px;
+    height: 240px;
+    border: 1px solid blue;
+    border-radius: 8px;
+    background: #fff;
+    overflow: auto;
+    display: flex;
+    flex-wrap: wrap;
+}
+.site_table td {
+    vertical-align: middle;
+}
+.site_table table {
 }
 </style>
